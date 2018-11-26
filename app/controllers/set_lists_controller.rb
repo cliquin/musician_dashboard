730f4eed20d5aeff3e16fad1e36248cf4e730ffb
Raghu Betina <raghu@firstdraft.com>
2018-11-26 01:58:53 +0000

    Only set_list user can modify set_list

--- app/controllers/set_lists_controller.rb
+++ app/controllers/set_lists_controller.rb
@@ -1,4 +1,14 @@
 class SetListsController < ApplicationController
+  before_action :current_user_must_be_set_list_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_set_list_user
+    set_list = SetList.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == set_list.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @set_lists = SetList.all
 
