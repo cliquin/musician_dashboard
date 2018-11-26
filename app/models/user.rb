f515879299cdea2b50c2ece8f20308b0ae2d90f8
Raghu Betina <raghu@firstdraft.com>
2018-11-26 01:57:51 +0000

    Direct association between users and statuses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :statuses,
+             :dependent => :destroy
+
   has_many   :set_lists,
              :dependent => :destroy
 
