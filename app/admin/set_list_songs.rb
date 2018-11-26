8c05363e5261d2982b2c253c46dde00713088de2
Raghu Betina <raghu@firstdraft.com>
2018-11-26 01:58:35 +0000

    Whitelist columns in SetListSong for ActiveAdmin

--- app/admin/set_list_songs.rb
+++ app/admin/set_list_songs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register SetListSong do
+
+ permit_params :song_id, :set_list_id, :position
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
